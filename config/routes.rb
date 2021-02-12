965807e7f3c2abe882cde20541d6a31b8b2b3b2b
Juan Carlos Concha <jnkarlos.c@gmail.com>
2021-02-12 12:33:18 -0300

    Added endpoint to update email a user #7

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, except: [:update]
     put '/users/:id/update_name', to: 'users#update'
+    put '/users/:id/update_email', to: 'users#update'
   end
 end
