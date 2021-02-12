301cfe6c5925696d9729e48391850daf9d1098aa
Juan Carlos Concha <jnkarlos.c@gmail.com>
2021-02-12 10:36:48 -0300

    Added endpoint to get a user by id #4

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,20 @@
 class Api::UsersController < ApplicationController
   before_action :user_all, only: [:index]
+  before_action :user_find, only: [:show]
 
   def index
     render json: @users
   end
 
+  def show
+    render json: @user
+  end
+
   private
+
+  def user_find
+    @user = user_all.find(params[:id])
+  end
 
   def user_all
     @users = User.all
